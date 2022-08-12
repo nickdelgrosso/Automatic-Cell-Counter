9578c16cae4ae67e4a973dc64957c01871b48cd8
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-08-12 14:02:55 +0200

    removed unused code

--- viewer.py
+++ viewer.py
@@ -10,14 +10,6 @@ from skimage.io import imread
 from skimage.measure import regionprops,label
 from CellCounter import get_binary_map,apply_opening,find_median_cell_size,apply_watershed
 
-# def str_to_bool(value):
-#     if isinstance(value, bool):
-#         return value
-#     if value.lower() in {'false', 'f', '0', 'no', 'n'}:
-#         return False
-#     elif value.lower() in {'true', 't', '1', 'yes', 'y'}:
-#         return True
-#     raise ValueError(f'{value} is not a valid boolean value')
 
 def get_args() -> CountCellArgs:
     parser = argparse.ArgumentParser(description='Automatic cell counter')
