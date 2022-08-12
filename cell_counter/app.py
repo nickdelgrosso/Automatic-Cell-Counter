6197d892e8d9fd5d334869b974d02be4033d9b5d
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-08-13 00:31:53 +0200

    name change

--- cell_counter/app.py
+++ cell_counter/app.py
@@ -8,7 +8,7 @@ class App:
     @property
     def count_cells(self) -> CountCellsWorkflow:
         from cell_counter.image_processors.image_processor import CellCounterImageProcessor
-        from cell_counter.io.image_repo import OSImageRepo
+        from cell_counter.repos.image_repo import OSImageRepo
         from cell_counter.viewers.napari_viewer import NapariImageViewer
         from cell_counter.workflows.count_cells import CountCellsWorkflow
         return CountCellsWorkflow(
